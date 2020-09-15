e22b700eff614ed2b415e25e0f45c1e6ec17ffb8
Robert (Bob) L. Jones <3848052+djrlj694@users.noreply.github.com>
2020-09-15 12:34:53 -0400

    feat(cookiecutter): Create post_gen_project.py

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys
+
+
+# =========================================================================== #
+# METADATA
+# =========================================================================== #
+
+__author__ = 'Robert (Bob) L. Jones'
+__credits__ = ['Robert (Bob) L. Jones']
+
+__copyright__ = 'Copyright 2020, Cookiecutter Make'
+__license__ = 'MIT'
+
+__created_date__ = 'Sep 15, 2020'
+__modified_date__ = 'Sep 15, 2020'
+
+
+# =========================================================================== #
+# CONSTANTS
+# =========================================================================== #
+
+# -- Debugging -- #
+
+DEBUG = True
+
+
+# =========================================================================== #
+# FUNCTIONS
+# =========================================================================== #
+
+# -- Main Program -- #
+
+def main():
+    """
+    Run the main set of functions that define the program.
+    """
+
+    print('DEBUG =', DEBUG)
+
+
+# =========================================================================== #
+# MAIN EXECUTION
+# =========================================================================== #
+
+# -- Main Execution -- #
+
+# If this module is in the main module, call the main() function.
+if __name__ == '__main__':
+    main()
+
+# -- Housekeeping -- #
+
+# Exit the program normally.
+sys.exit(0)
